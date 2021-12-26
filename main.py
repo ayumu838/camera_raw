de17d4f59b4c109701e070a18c0ea67df760a0d9
ayumu.mochizuki <ayumu839.a@gmail.com>
2021-12-26 16:58:42 +0900

    ブラックレベルの補正を追加

--- main.py
+++ main.py
@@ -79,4 +79,35 @@ plt.figure(figsize=(8, 8))
 plt.imshow(dms_img)
 plt.axis('off')
 plt.title(u' ホワイトバランス後の画像')
+# plt.show()
+
+# blc = raw.black_level_per_channel
+blc = [66, 66, 66, 66]
+print(blc)
+
+pattern = raw.raw_pattern
+blc_raw = raw_array.astype('int')
+
+for y in range(0, h, 2):
+  for x in range(0, w, 2):
+    blc_raw[y + 0, x + 0] -= blc[pattern[0,0]]
+    blc_raw[y + 0, x + 1] -= blc[pattern[0,1]]
+    blc_raw[y + 1, x + 0] -= blc[pattern[1,0]]
+    blc_raw[y + 1, x + 1] -= blc[pattern[1,1]]
+
+print("ブラックレベルの補正前: 最小値=", raw_array.min(), "最大値=", raw_array.max())
+print("ブラックレベルの補正後: 最小値=", blc_raw.min(), "最大値=", blc_raw.max())
+
+gain, colors = raw.camera_whitebalance, raw.raw_colors
+wb_img = white_balance.white_balance(blc_raw, gain, colors)
+dms_img = demosaic.simple_demosaic(wb_img, raw.raw_pattern)
+
+plt.figure(figsize=(8, 8))
+dms_img /= 1024
+dms_img[dms_img < 0] = 0
+dms_img[dms_img > 1] = 1.0
+
+plt.imshow(dms_img)
+plt.axis('off')
+plt.title(u' ブラックレベル補正後の画像')
 plt.show()
