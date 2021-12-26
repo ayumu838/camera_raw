bf7477ac899e738d7a72c3c1618a4e2bf00e059f
ayumu.mochizuki <ayumu839.a@gmail.com>
2021-12-26 12:11:34 +0900

    デモザイクの処理を高速化

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*-
 
+from matplotlib import widgets
 import rawpy, imageio
 import numpy as np
 import matplotlib.pyplot as plt
+
+def simple_demosaic(raw_array, pattern):
+  """
+  簡易デモザイク処理を行う。
+  Parameters
+  ----------
+  raw_array: numpy array
+    入力BayerRAW画像データ
+  pattern: int[2, 2]
+    Bayerパターン 0:red, 1:green, 2: blue, 3:green
+
+  Returns
+  -------
+  dms_img: numpy array
+  出 力RGB画像 サイズは入力の縦横共に1/2"""
+
+  height, width = raw_array.shape
+  dms_img = np.zeros((height // 2, width // 2, 3))
+  pattern[pattern == 3] = 1
+  dms_img[:, :, pattern[0, 0]] = raw_array[0::2, 0::2]
+  dms_img[:, :, pattern[0, 1]] = raw_array[0::2, 1::2]
+  dms_img[:, :, pattern[1, 0]] = raw_array[1::2, 0::2]
+  dms_img[:, :, pattern[1, 1]] = raw_array[1::2, 1::2]
+  return dms_img
 
 raw_file = "chart.jpg"
 raw = rawpy.imread(raw_file)
@@ -50,18 +75,8 @@ plt.axis('off')
 plt.title(u'RAW画像の各要素に色を割り当てたものを拡大表示')
 # plt.show()
 
-pattern = raw.raw_pattern
-pattern[pattern == 3] = 1
-print(pattern)
 
-dms_img = np.zeros((h//2, w//2, 3))
-for y in range(0, h, 2):
-  for x in range(0, w, 2):
-    dms_img[y // 2, x //2, pattern[0, 0]] += raw_array[y + 0, x + 0]
-    dms_img[y // 2, x //2, pattern[0, 1]] += raw_array[y + 0, x + 1]
-    dms_img[y // 2, x //2, pattern[1, 0]] += raw_array[y + 1, x + 0]
-    dms_img[y // 2, x //2, pattern[1, 1]] += raw_array[y + 1, x + 1]
-    dms_img[y // 2, x // 2, 1] /= 2
+dms_img = simple_demosaic(raw_array, raw.raw_pattern)
 
 dms_img[dms_img < 0] = 0
 dms_img /= 1024
